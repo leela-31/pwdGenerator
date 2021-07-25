fd865083b2235b6fa39488971fe99d97ecb67b0d
leela-31 <65697691+leela-31@users.noreply.github.com>
2021-07-25 11:58:13 +0530

    Add files via upload

---
+++ pwdGenerator.py
@@ -0,0 +1,22 @@
+from random import  *
+from string import  *
+import pyperclip as pyp
+
+chars = []
+chars.extend(ascii_letters)
+chars.extend(digits)
+chars.extend(punctuation)
+
+no_of_pwd = int(input('Enter no.of passwords required: '))
+pwd_len = int(input('Enter password(s) length: '))
+
+for pwds in range(no_of_pwd):
+	pwd = ''
+	for ch in range(pwd_len):
+		pwd = pwd + choice(chars)
+	pyp.copy(pwd) 
+	print(pwd)
+
+
+
+	
